8eb8e8e22dff3b78bdc1f4415f7f3ea3999eecc6
FridahNtika <fn101@wellesley.edu>
2024-05-28 15:50:49 -0400

    created an avatar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { Outlet } from "react-router-dom"
 import React from 'react';
-import { ChakraProvider, Box } from '@chakra-ui/react';
+import { ChakraProvider, Box, Avatar } from '@chakra-ui/react';
 import { Navbar } from './Navbar.jsx';
+import { AppAvatar } from './components/Avatar.jsx';
 import './App.css';
 
 export const App = () => {
