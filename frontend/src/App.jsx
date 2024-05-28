63f04ec638c12d6a957b1966fecb224069f1ff68
HarunAhmed17 <hahmed3173@gmail.com>
2024-05-28 16:43:30 -0400

    initial datagrid structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,12 @@ import { Container } from "@chakra-ui/react";
 import './App.css';
 import SimpleSidebar from "./components/Sidebar";
 
+
 export const App = () => {
   return (
     <ChakraProvider>
         <main>
+          <DataGrid></DataGrid>
           <SimpleSidebar>
           <Container display="grid" minHeight="95vh" padding="0">
             <Outlet />
