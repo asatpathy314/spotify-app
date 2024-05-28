3c412ad8eea4744a82fac50e397d9f1a5bf7bdde
Riley Washington <rileywash02@gmail.com>
2024-05-28 17:30:41 -0400

    pushing changes so they are in main for others

--- frontend/src/components/messages.jsx
+++ frontend/src/components/messages.jsx
@@ -1,3 +1,14 @@
-import { Avatar } from '@chakra-ui/react';
 import { Box, Wrap, WrapItem } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import '..stylesheets/Messages.css'
 
+function Messages({userID}) {
+    const[userID, setUserID] = useState("");
+
+    // useEffect(() => {
+    //     const 
+    // })
+
+};
+
+export default Messages
