b82f86d940a9d290cfd6e50f5a7448ed92b9ec4f
asatpathy314 <asatpathy314@gmail.com>
2024-05-28 10:33:58 -0400

    initialized firebase

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+// Initialize Express app
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+//Initialize CORS
+var cors = require('cors')
+app.use(cors())
+
+// Import Routes
+
+// Start the server
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
