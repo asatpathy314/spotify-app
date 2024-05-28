4d19243f5b70705f2320632fd9e28841963b05eb
asatpathy314 <asatpathy314@gmail.com>
2024-05-28 11:29:50 -0400

    router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom"
+ 
 const App = () => {
   // 2. Wrap ChakraProvider at the root of your app
   return (
-      <h1>hello</h1>
+      <main>
+        <Outlet />
+      </main>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,30 @@ import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import * as ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+      path: '/',
+      element: <h1>login</h1>
+      },
+      {
+        path: '/profile',
+        element: <h1>profile</h1>
+      }
+    ]
+  }
+])
 
 const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router}/>
     </ChakraProvider>
   </React.StrictMode>,
 )
