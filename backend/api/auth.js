f8b4a671629f1fd9217c1c47be8b932deb26b5fb
asatpathy314 <asatpathy314@gmail.com>
2024-05-29 13:42:32 -0400

    committed api route

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -25,7 +25,7 @@ app.get('/login', function(req, res) {
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-top-read user-read-recently-played';
+  var scope = 'user-read-private user-read-email user-top-read user-read-recently-played user-library-read';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
