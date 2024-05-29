6207780a0920f66c678851ee73a2813602ee046c
asatpathy314 <asatpathy314@gmail.com>
2024-05-29 12:02:02 -0400

    fixed issues with auth.js and added a user.js route

--- frontend/src/routes/Profile.jsx
+++ frontend/src/routes/Profile.jsx
@@ -1,112 +1,117 @@
-import { useContext, useEffect, useState } from "react"
-import { useSearchParams, useParams } from "react-router-dom"
-import { AuthContext } from "../components/AuthProvider"
-import { AppAvatar } from "../components/AppAvatar"
-import { Heading, Grid, GridItem, Avatar, Stack, Text, Img } from "@chakra-ui/react"
-import { App } from "../App"
-import axios from "axios"
+import { useContext, useEffect, useState } from "react";
+import { useSearchParams, useParams } from "react-router-dom";
+import { AuthContext } from "../components/AuthProvider";
+import { AppAvatar } from "../components/AppAvatar";
+import {
+  Heading,
+  Grid,
+  GridItem,
+  Avatar,
+  Stack,
+  Text,
+  Img,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 const Profile = () => {
-    const { token, setToken, userID, setUserID } = useContext(AuthContext)
-    const [searchParams, setSearchParams] = useSearchParams();
-    const [favoriteArtist, setFavoriteArtist] = useState(null);
-    const [isEditable, setIsEditable] = useState(false);
-    const [favoriteSong, setFavoriteSong] = useState(null);
-    const [forbidden, setForbidden] = useState(null);
-    const { id } = useParams();
+  const { token, setToken, userID, setUserID } = useContext(AuthContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [profileData, setProfileData] = useState(null);
+  const [isEditable, setIsEditable] = useState(false);
+  const [forbidden, setForbidden] = useState(null);
+  const { id } = useParams();
 
-    useEffect(() => {
-        if (id == userID) {
-            setIsEditable(true);
-            console.log(isEditable);
-        }
-        if (token===null || userID===null) {
-            if (searchParams.get('access_token') && searchParams.get('user_id')) {
-                setToken(searchParams.get('access_token'));
-                setUserID(searchParams.get('user_id'));
-            }
-            else {
-                setForbidden(true);
-                window.location.replace("/forbidden");
-                return;
-            }
-        }
-
-        }, [id, userID, token, searchParams, setToken, setUserID, isEditable])
-    console.log(favoriteSong)
-    console.log('hmm', favoriteArtist)
-    if (!forbidden) {
-        return (
-            <Grid
-                h='90%'
-                templateRows='repeat(2, 1fr)'
-                templateColumns='repeat(4, 1fr)'
-                color="#FFFFFE"
-                gap={4}
-            >
-                <GridItem colSpan={4} bg='#0f0e17' padding={10}>
-                    <Stack direction={['column', 'row', 'row', 'row']} spacing={4}>
-                        <Avatar
-                            src='https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250'
-                            size={['3xl', '3xl', '3xl', '3xl']}
-                        />
-                        <div>
-                            <Heading size={['xl', null, null, '2xl']} color='#FFFFFE'>
-                                User
-                            </Heading>
-                            <Heading size={['md', null, null, 'lg']} color='#FFFFFE'>
-                                daniel341
-                            </Heading>
-                            <Text fontSize='xl'>
-                                Hiii! My name is Daniel I love chicken and rice. Hiii! My name is
-                                Daniel I love chicken and rice.
-                            </Text>
-                        </div>
-                    </Stack>
-                </GridItem>
-                <GridItem colSpan={2} bg='#0f0e17' padding={10}>
-                    <Heading size={['sm', 'md', null, 'lg']}>
-                        My Favorite Song
-                    </Heading>
-                    {favoriteSong === null && <Text>Loading...</Text>}
-                    {favoriteSong !== null && (
-                        <>
-                            <Img
-                                src={favoriteSong.album.images[1].url}
-                                alt={favoriteSong.name}
-                                mx='auto' // Add this line to center the image horizontally
-                                mt='5'
-                                mb='5'
-                            />
-                            <Heading size={['sm', 'md', null, 'md']} textAlign="center">
-                                {favoriteSong.name}
-                            </Heading>
-                        </>
-                    )}
-                </GridItem>
-                <GridItem colSpan={2} bg='#0f0e17' padding={10}>
-                    <Heading size={['sm', 'md', null, 'lg']}>
-                        My Favorite Artist
-                    </Heading>
-                    {favoriteArtist === null && <Text>Loading...</Text>}
-                    {favoriteArtist !== null && (
-                        <>
-                            <Img
-                                src={favoriteArtist.images[1].url}
-                                alt={favoriteArtist.name}
-                                mx='auto' // Add this line to center the image horizontally
-                                mt='5'
-                                mb='5'
-                            />
-                            <Heading size={['sm', 'md', null, 'md']} textAlign="center">
-                                {favoriteArtist.name}
-                            </Heading>
-                        </>
-                    )}
-                </GridItem>
-            </Grid>
-        )
+  useEffect(() => {
+    if (id == userID) {
+      setIsEditable(true);
+      console.log(isEditable);
     }
-}
+    if (token === null || userID === null) {
+      if (searchParams.get("access_token") && searchParams.get("user_id")) {
+        setToken(searchParams.get("access_token"));
+        setUserID(searchParams.get("user_id"));
+      } else {
+        setForbidden(true);
+        window.location.replace("/forbidden");
+        return;
+      }
+    }
+    if (id) {
+        axios.get("http://localhost:8000/user?id=" + id)
+            .then((res) => {
+                setProfileData(res.data);
+            })
+    }
+  }, [id, userID, token, searchParams, setToken, setUserID, isEditable]);
+  if (!forbidden) {
+    return (
+      <Grid
+        h="90%"
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(4, 1fr)"
+        color="#FFFFFE"
+        gap={4}
+      >
+        <GridItem colSpan={4} bg="#0f0e17" padding={10}>
+          <Stack direction={["column", "row", "row", "row"]} spacing={4}>
+            <Avatar
+              src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
+              size={["3xl", "3xl", "3xl", "3xl"]}
+            />
+            <div>
+              <Heading size={["xl", null, null, "2xl"]} color="#FFFFFE">
+                User
+              </Heading>
+              <Heading size={["md", null, null, "lg"]} color="#FFFFFE">
+                {profileData && profileData.name}
+                {!profileData && "Loading..."}
+              </Heading>
+              <Text fontSize="xl">
+                Hiii! My name is Daniel I love chicken and rice. Hiii! My name
+                is Daniel I love chicken and rice.
+              </Text>
+            </div>
+          </Stack>
+        </GridItem>
+        <GridItem colSpan={2} bg="#0f0e17" padding={10}>
+          <Heading size={["sm", "md", null, "lg"]}>My Favorite Song</Heading>
+          {profileData === null && <Text>Loading...</Text>}
+          {profileData !== null && (
+            <>
+              <Img
+                src={profileData.favoriteSong.album.images[1].url}
+                alt={profileData.favoriteSong.name}
+                mx="auto" // Add this line to center the image horizontally
+                mt="5"
+                mb="5"
+              />
+              <Text fontSize={["lg", "2xl", null, "2xl"]} textAlign="center">
+                {profileData.favoriteSong.name}
+              </Text>
+            </>
+          )}
+        </GridItem>
+        <GridItem colSpan={2} bg="#0f0e17" padding={10}>
+          <Heading size={["sm", "md", null, "lg"]}>My Favorite Artist</Heading>
+          {profileData === null && <Text>Loading...</Text>}
+          {profileData !== null && (
+            <>
+              <Img
+                src={profileData.favoriteArtist.images[1].url}
+                alt={profileData.favoriteArtist.name}
+                mx="auto" // Add this line to center the image horizontally
+                mt="5"
+                mb="5"
+              />
+              <Text fontSize={["lg", "2xl", null, "2xl"]} textAlign="center">
+                {profileData.favoriteArtist.name}
+              </Text>
+            </>
+          )}
+        </GridItem>
+      </Grid>
+    );
+  }
+};
 
-export default Profile
+export default Profile;
