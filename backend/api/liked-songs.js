adab51f45b03edf7d8ed9d6dd41fc7c6cd98e0e1
FridahNtika <fn101@wellesley.edu>
2024-05-29 13:24:28 -0400

    creates liked songs api route

---
+++ backend/api/liked-songs.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const querystring = require('querystring');
+const request = require('request');
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    
+})
+
+module.exports = router;
