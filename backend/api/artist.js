eacfd705799dc35e053b42dbb005fe468fceb3bd
asatpathy314 <asatpathy314@gmail.com>
2024-05-29 09:17:00 -0400

    documented api endpoints for song and artist

--- backend/api/artist.js
+++ backend/api/artist.js
@@ -3,14 +3,31 @@ const querystring = require('querystring');
 const request = require('request');
 const router = express.Router();
 
+/**
+ * @api {get} / Get User's Top Artists
+ * @apiName GetTopArtists
+ * @apiGroup Spotify
+ * 
+ * @apiParam {String} spotify_token Spotify access token.
+ * @apiParam {Number} [num_artists=10] Number of top artists to retrieve (default is 10).
+ * @apiParam {String} timeframe Time range for top artists. Valid values are "short_term", "medium_term", and "long_term".
+ * 
+ * @apiSuccess {Object} data JSON object containing the user's top artists.
+ * 
+ * @apiError {Object} 400 Missing required parameter.
+ * @apiError {Object} 500 Failed to fetch top artists.
+ * 
+ */
 router.get('/', (req, res) => {
     const spotify_token = req.query.spotify_token;
-    const num_artists = req.query.num_artists || 10; // Default to 10 songs
+    const num_artists = req.query.num_artists || 10; // Default to 10 artists
     const timeframe = req.query.timeframe;
     const offset = 0;
+
     if (!timeframe || !spotify_token || (timeframe !== "short_term" && timeframe !== "medium_term" && timeframe !== "long_term")) {
         return res.status(400).json({ error: "Missing required parameter" });
     }
+
     const type = "artists";
 
     const options = {
@@ -27,7 +44,7 @@ router.get('/', (req, res) => {
             return res.status(500).json({ error: "Failed to fetch top artists" });
         }
         const data = JSON.parse(body);
-        console.log(data)
+        console.log(data);
         res.status(200).json(data);
     });
 });
