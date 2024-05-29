5832429fa7d2ebb880c26c1721f9a4d73c6ff22f
keningzhao99 <117388581+keningzhao99@users.noreply.github.com>
2024-05-29 12:37:33 +0100

    finished initial front-end design

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,17 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
-function Discover() {
+function Profile({ userName, profilePictureUrl }) {
+    const [isHovered, setIsHovered] = useState(false);
+    const [backgroundColor, setBackgroundColor] = useState(getRandomColor());
+
+    useEffect(() => {
+        setBackgroundColor(getRandomColor());
+    }, []);
+
+    const containerStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'left', // Align both image and text horizontally
+        transition: 'transform 0.3s ease-in-out', // Adding transition for smooth effect
+        transform: isHovered ? 'scale(1.1)' : 'scale(1)'
+    };
+
+    const imgStyle = {
+        width: '180px',
+        height: '180px',
+        borderRadius: '50%',
+        objectFit: 'cover',
+    };
+
+    const noImgStyle = {
+        ...imgStyle,
+        backgroundColor: backgroundColor // Use the randomized color here
+    };
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
 
     return (
-        <>
-        <div>
-            <h1>Discover</h1>
+        <div
+            style={containerStyle}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            {profilePictureUrl ? (
+                <img src={profilePictureUrl} alt={`${userName}'s profile`} style={imgStyle} />
+            ) : (
+                <div style={noImgStyle}></div>
+            )}
+            <b style={{ fontSize: '22px', marginTop: '10px', marginBottom: '5px' }}>{userName}</b>
+            <p style={{ margin: 0 }}>Profile</p>
         </div>
-
-
-        </>
-    )
+    );
 }
 
-export default Discover;
+export default Profile;
+
+function getRandomColor() {
+    const colors = ['#F1A819', '#E53170'];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
