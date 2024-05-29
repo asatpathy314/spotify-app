d5c8fcdde1d74516414613c22d0c49339cc14b55
FridahNtika <fn101@wellesley.edu>
2024-05-29 16:57:46 -0400

    prints all 50 liked songs

--- frontend/src/routes/LikedSongs.jsx
+++ frontend/src/routes/LikedSongs.jsx
@@ -3,7 +3,7 @@ import { useContext, useState, useEffect } from "react";
 import axios from 'axios';
 import { useSearchParams} from "react-router-dom"
 import { AuthContext } from "../components/AuthProvider"
-import { Container, Card, Image, Heading, Stack, Box, Text } from "@chakra-ui/react";
+import { Container, Card, Image, Heading, SimpleGrid, Stack, Box, Text } from "@chakra-ui/react";
 
 export const LikedSongs = () => {
     const [songs, setSongs] = useState([]);
@@ -31,26 +31,36 @@ export const LikedSongs = () => {
         }
     }, [token]);
 
+    const bundleSongs = (array, size) => {
+        const bundled = [];
+        for (let i = 0; i < array.length; i += size) {
+            bundled.push(array.slice(i, i + size));
+        }
+        return bundled;
+    };
+
+    const groups = bundleSongs(songs, 5);
+
       return (
-       <Container display="grid" padding='20px'>
+        <>
             <Heading color="#FFFFFE">Your Liked Songs</Heading>
             {forbidden ? (
                 <Text color="red.500">You do not have permission to view this content.</Text>
             ) : (
-                <Stack direction='row' spacing={4} marginBottom={4} alignItems="flex-start">
-                    {songs.map((song, index) => (
-                        <Card backgroundColor="#0F0E17" width="100%" minWidth="200px" height="20%" minHeight="150px" key={index}>
-                            <Box padding="4" display='flex' flexWrap='wrap'>
-                                <Image boxSize='200px' objectFit='cover'
-                                    src={song.track.album.images[0].url || 'https://via.placeholder.com/150'} alt={index}
-                                />
-                                <Text color="#FFFFFE" textAlign="center" marginTop="2">{song.track.name}</Text>
-                            </Box>
-                        </Card>
-                    ))}
-                </Stack>
+                <SimpleGrid columns={5} spacing='40px'>
+                        {songs.map((song, index) => (
+                            <Card backgroundColor="#0F0E17" width="100%" minWidth="200px" height="20%" minHeight="150px" key={index}>
+                                <Box padding="4" display='flex' flexWrap='wrap'>
+                                    <Image boxSize='200px' objectFit='cover'
+                                       src={song.track.album.images[0].url || 'https://via.placeholder.com/150'} alt={index}
+                                    />
+                                    <Text color="#FFFFFE" textAlign="center" marginTop="2">{song.track.name}</Text>
+                                </Box>
+                            </Card>
+                        ))}
+                    </SimpleGrid>
             )}
-        </Container>
+            </>
       )
 
     /* 
