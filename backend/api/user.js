3a11c06df10934ddb5dac4732d984ae8a27d0a8f
asatpathy314 <asatpathy314@gmail.com>
2024-05-29 13:30:59 -0400

    users route

--- backend/api/user.js
+++ backend/api/user.js
@@ -26,4 +26,27 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get("/all", async(req, res) => {
+    try {
+        // Access the query parameter "id"
+        const userId = req.query.id;
+        
+        if (!userId) {
+            return res.status(400).send("Missing query parameter: id");
+        }
+
+        // Reference to the user document
+        const usersSnapshot = await db.collection("user").get()
+        const users = [];
+        usersSnapshot.forEach(doc => {
+            users.push({...doc.data(), id: doc.id});
+        });
+        res.status(200).json(users);
+        
+    } catch (error) {
+        console.error("Error retrieving user:", error);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
 module.exports = router;
