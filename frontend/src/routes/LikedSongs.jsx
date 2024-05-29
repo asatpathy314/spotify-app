3a581fc789fa670e7f08e149d2481c9c88bcb86f
FridahNtika <fn101@wellesley.edu>
2024-05-29 14:30:56 -0400

    updates

--- frontend/src/routes/LikedSongs.jsx
+++ frontend/src/routes/LikedSongs.jsx
@@ -13,45 +13,47 @@ export const LikedSongs = () => {
     
     const fetchSongs = async () => {
         try {
-            const responses = await axios.get("http://localhost:8000/liked");
+            console.log("")
+            const responses = await axios.get("http://localhost:8000/liked", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
             console.log(responses.data);
             setSongs(responses.data);
         } catch (error) {
             console.log("Error getting the songs: ", error);
+            if (error.response && error.response.status === 403) {
+                setForbidden(true);
+            };
         };
     }
 
     useEffect(() => {
-        fetchSongs()
-    }, []);
-
-      const splitArray = (array, size) => {
-        const split = [];
-        for (let i = 0; i < array.length; i += size) {
-            split.push(array.slice(i, i + size));
+        if (token) {
+            fetchSongs()
         }
-        return split;
-    }
-
-    const split = splitArray(songs, 4);
+    }, []);
 
       return (
         <Container minHeight="100vh" display="grid">
             <Heading color="#FFFFFE">Your Liked Songs</Heading>
-            {split.map((chunk, chunkIndex) => (
-                <Stack direction='row' spacing={4} key={chunkIndex} marginBottom={4} alignItems="flex-start">
-                    {chunk.map((user, userIndex) => (
-                        <Card backgroundColor="#0F0E17" width="100%" minWidth="200px" height="20%" minHeight="150px" key={userIndex}>
+            {forbidden ? (
+                <Text color="red.500">You do not have permission to view this content.</Text>
+            ) : (
+                <Stack direction='row' spacing={4} marginBottom={4} alignItems="flex-start">
+                    {songs.map((song, index) => (
+                        <Card backgroundColor="#0F0E17" width="100%" minWidth="200px" height="20%" minHeight="150px" key={index}>
                             <Box padding="4" textAlign='left'>
                                 <Image boxSize='200px' objectFit='cover'
-                                    src={user.profilePictureUrl || 'https://via.placeholder.com/150'} alt={user.userName}
+                                    src={song || 'https://via.placeholder.com/150'} alt={index}
                                 />
                                 <Text color="#FFFFFE" textAlign="center" marginTop="2">{user.userName}</Text>
                             </Box>
                         </Card>
                     ))}
                 </Stack>
-            ))}
+            )}
         </Container>
       )
 
