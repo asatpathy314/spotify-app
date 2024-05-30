9009aced10335d96d1789bc7d19a678c760ebf5d
asatpathy314 <asatpathy314@gmail.com>
2024-05-30 09:32:11 -0400

    fixed app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,6 @@ app.use(cookieParser())
 
 //Import Routes
 //const artists = require("./api/getArtists");
-<<<<<<< HEAD
 const auth = require("./api/auth");
 const forum = require("./api/forum");
 const song = require("./api/song");
@@ -20,27 +19,14 @@ const messages = require("./api/messages");
 const user = require("./api/user");
 
 
-=======
-const auth = require("./api/auth")
-const user = require("./api/user");
-const forum = require("./api/forum");
-const song = require("./api/song");
-const artist = require("./api/artist");
-//const liked = require("./api/liked-songs");
->>>>>>> main
 
 //initialize Routes
 app.use('/auth', auth);
 app.use('/forum', forum);
 app.use('/song', song);
 app.use('/artist', artist);
-<<<<<<< HEAD
 app.use('/messages', messages);
 
-=======
-//app.use('/liked', song);
-app.use('/user', user);
->>>>>>> main
 
 // Start the server
 const port = process.env.PORT || 8000;
