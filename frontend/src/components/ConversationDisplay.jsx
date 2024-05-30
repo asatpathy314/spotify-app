3f8c0ea4f5a063a8acbf6c5bacb5e455d378757c
asatpathy314 <asatpathy314@gmail.com>
2024-05-30 17:29:07 -0400

    finished changes with the messaging system (added timestamps and sorting by time)

--- frontend/src/components/ConversationDisplay.jsx
+++ frontend/src/components/ConversationDisplay.jsx
@@ -1,43 +1,49 @@
-//component to render horizontal list of users to message
-
-import { Card, Image, Stack, CardBody, Heading, CardFooter, Text, Button, Avatar, Link } from '@chakra-ui/react';
+import { Card, Image, Stack, CardBody, Heading, CardFooter, Text, Button, Avatar, Link, Box } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const ConversationDisplay = ( { userID, keyValue, conversation } ) => {
+const ConversationDisplay = ({ userID, keyValue, conversation }) => {
     const [userData, setUserData] = useState([]);
     const [latestMessage, setLatestMessage] = useState("");
-    
+
     useEffect(() => {
-        axios.get('http://localhost:8000/user?id='+userID)
+        axios.get('http://localhost:8000/user?id=' + userID)
             .then((res) => setUserData(res.data))
-            .catch((err) => console.error(err))
-        setLatestMessage(conversation.messages[conversation.messages.length-1])
+            .catch((err) => console.error(err));
+        setLatestMessage(conversation.messages[conversation.messages.length - 1]);
     }, [userID, conversation]);
 
+    const formatDate = (timestamp) => {
+        const date = new Date(timestamp * 1000); // Convert seconds to milliseconds
+        return date.toLocaleString();
+    };
 
-return (
-    <Link key={1000-keyValue} href={`/messages/${conversation.id}`}>
-        <Card
-            direction='row'
-            overflow='hidden'
-            bg='#0f0e17'
-            borderRadius={100}
-            >   
-                <Avatar src={userData.profile} size={'2xl'} margin='5'/>
-                <CardBody color = "white">
+    return (
+        <Link key={1000 - keyValue} href={`/messages/${conversation.id}`}>
+            <Card
+                direction='row'
+                overflow='hidden'
+                bg='#0f0e17'
+                borderRadius={100}
+                position="relative"
+            >
+                <Avatar src={userData.profile} size={'2xl'} margin='5' />
+                <CardBody color="white" p={6}>
                     <Stack>
-                        <Heading>
-                            {userData.name}
-                        </Heading>
+                        <Stack direction={{ base: "column", md: "row" }} justifyContent="space-between" alignItems={{ base: "flex-start", md: "center" }}>
+                            <Heading>
+                                {userData.name}
+                            </Heading>
+                            <Text>{latestMessage && formatDate(latestMessage.timestamp._seconds)}</Text>
+                        </Stack>
                         <Text>
-                            {latestMessage && latestMessage.user._path.segments[1].trim() !== userID ? "You: " : ""}{latestMessage.text} 
+                            {latestMessage && latestMessage.user._path.segments[1].trim() !== userID ? "You: " : ""}{latestMessage.text}
                         </Text>
                     </Stack>
                 </CardBody>
-        </Card>
-    </Link>
-    )
-}
+            </Card>
+        </Link>
+    );
+};
 
 export default ConversationDisplay;
